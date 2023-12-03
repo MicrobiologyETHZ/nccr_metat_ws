471677ab677e1b9f1db61e09489740aa8ada591d
ASintsova <ansintsova@biol.ethz.ch>
2023-12-03 21:34:48 +0100

    updating versions for binder

--- install.R
+++ install.R
@@ -4,5 +4,5 @@ install.packages(c("tidyverse", "stats", "stats4", "caret", "ggplot2"))
 if (!require("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
-BiocManager::install(version = "3.18")
+BiocManager::install(version = "3.16")
 BiocManager::install("DESeq2")
